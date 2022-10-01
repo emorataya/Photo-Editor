b2e3409cc8214a89d76edf576e2bad5533914b95
emorataya <emorataya@harding.edu>
2022-10-01 18:39:45 -0500

    Update Edit Form
    Add brightness trackbar, color, save and cancel buttons to Edit Form.
    
    Add functionality to brightness trackbar, color button.

--- PhotoEditor/PhotoEditor/EditPhotoForm.Designer.cs
+++ PhotoEditor/PhotoEditor/EditPhotoForm.Designer.cs
@@ -30,21 +30,35 @@
         {
             this.photoPictureBox = new System.Windows.Forms.PictureBox();
             this.invertButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.brightnessTrackBar = new System.Windows.Forms.TrackBar();
+            this.colorButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             ((System.ComponentModel.ISupportInitialize)(this.photoPictureBox)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.brightnessTrackBar)).BeginInit();
             this.SuspendLayout();
             // 
             // photoPictureBox
             // 
+            this.photoPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.photoPictureBox.Location = new System.Drawing.Point(12, 12);
             this.photoPictureBox.Name = "photoPictureBox";
-            this.photoPictureBox.Size = new System.Drawing.Size(371, 350);
+            this.photoPictureBox.Size = new System.Drawing.Size(589, 377);
             this.photoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.photoPictureBox.TabIndex = 0;
             this.photoPictureBox.TabStop = false;
             // 
             // invertButton
             // 
-            this.invertButton.Location = new System.Drawing.Point(307, 368);
+            this.invertButton.Location = new System.Drawing.Point(490, 46);
             this.invertButton.Name = "invertButton";
             this.invertButton.Size = new System.Drawing.Size(75, 23);
             this.invertButton.TabIndex = 1;
@@ -52,16 +66,103 @@
             this.invertButton.UseVisualStyleBackColor = true;
             this.invertButton.Click += new System.EventHandler(this.invertButton_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.brightnessTrackBar);
+            this.groupBox1.Controls.Add(this.colorButton);
+            this.groupBox1.Controls.Add(this.invertButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 395);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(589, 104);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Brightness";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Light";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dark";
+            // 
+            // brightnessTrackBar
+            // 
+            this.brightnessTrackBar.Location = new System.Drawing.Point(38, 46);
+            this.brightnessTrackBar.Maximum = 100;
+            this.brightnessTrackBar.Name = "brightnessTrackBar";
+            this.brightnessTrackBar.Size = new System.Drawing.Size(147, 45);
+            this.brightnessTrackBar.TabIndex = 3;
+            this.brightnessTrackBar.Value = 50;
+            this.brightnessTrackBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.brightnessTrackBar_MouseUp);
+            // 
+            // colorButton
+            // 
+            this.colorButton.Location = new System.Drawing.Point(296, 46);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(75, 23);
+            this.colorButton.TabIndex = 2;
+            this.colorButton.Text = "Color...";
+            this.colorButton.UseVisualStyleBackColor = true;
+            this.colorButton.Click += new System.EventHandler(this.colorButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.Location = new System.Drawing.Point(436, 505);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Location = new System.Drawing.Point(526, 505);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
             // EditPhotoForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(394, 404);
-            this.Controls.Add(this.invertButton);
+            this.ClientSize = new System.Drawing.Size(613, 540);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.photoPictureBox);
             this.Name = "EditPhotoForm";
             this.Text = "Edit Photo";
             ((System.ComponentModel.ISupportInitialize)(this.photoPictureBox)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.brightnessTrackBar)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -70,5 +171,14 @@
 
         public PictureBox photoPictureBox;
         private Button invertButton;
+        private GroupBox groupBox1;
+        private Button saveButton;
+        private Button cancelButton;
+        private TrackBar brightnessTrackBar;
+        private Button colorButton;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private ColorDialog colorDialog1;
     }
 }
--- PhotoEditor/PhotoEditor/EditPhotoForm.cs
+++ PhotoEditor/PhotoEditor/EditPhotoForm.cs
@@ -43,5 +43,63 @@ namespace PhotoEditor
                 }
             }
         }
+
+        private void colorButton_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+
+            var transformedBitmap = new Bitmap(photoPictureBox.Image);
+
+            // This could take a long time... should be done in a thread
+            AlterColors(transformedBitmap, colorDialog1.Color);
+
+            photoPictureBox.Image = transformedBitmap;
+        }
+
+        private void AlterColors(Bitmap transformedBitmap, Color chosenColor)
+        {
+            for (int y = 0; y < transformedBitmap.Height; y++)
+            {
+                for (int x = 0; x < transformedBitmap.Width; x++)
+                {
+                    var color = transformedBitmap.GetPixel(x, y);
+                    int ave = (color.R + color.G + color.B) / 3;
+                    double percent = ave / 255.0;
+                    int newRed = Convert.ToInt32(chosenColor.R * percent);
+                    int newGreen = Convert.ToInt32(chosenColor.G * percent);
+                    int newBlue = Convert.ToInt32(chosenColor.B * percent);
+                    var newColor = Color.FromArgb(newRed, newGreen, newBlue);
+                    transformedBitmap.SetPixel(x, y, newColor);
+                }
+            }
+        }
+
+        private void brightnessTrackBar_MouseUp(object sender, MouseEventArgs e)
+        {
+            var transformedBitmap = new Bitmap(photoPictureBox.Image);
+
+            ChangeBrightness(transformedBitmap, brightnessTrackBar.Value);
+
+            photoPictureBox.Image = transformedBitmap;
+        }
+
+        // brightness is a value between 0 – 100. Values < 50 makes the image darker, > 50 makes lighter
+        private void ChangeBrightness(Bitmap transformedBitmap, int brightness)
+        {
+            // Calculate amount to change RGB
+            int amount = Convert.ToInt32(2 * (50 - brightness) * 0.01 * 255);
+            for (int y = 0; y < transformedBitmap.Height; y++)
+            {
+                for (int x = 0; x < transformedBitmap.Width; x++)
+                {
+                    var color = transformedBitmap.GetPixel(x, y);
+                    int newRed = Math.Max(0, Math.Min(color.R - amount, 255));
+                    int newGreen = Math.Max(0, Math.Min(color.G - amount, 255));
+                    int newBlue = Math.Max(0, Math.Min(color.B - amount, 255));
+                    var newColor = Color.FromArgb(newRed, newGreen, newBlue);
+                    transformedBitmap.SetPixel(x, y, newColor);
+                }
+            }
+        }
     }
 }
